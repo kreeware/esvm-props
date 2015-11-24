5ea7ca78a09dafacca75d1fa0ea108d300df9f77
spalger <email@spalger.com>
2015-11-24 17:52:44 -0600

    hard code the urls for now

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,11 @@ var express = require('express');
 var router = express.Router();
 var get = require('https').get;
 
+var urlsPath = require('path').resolve(__dirname, 'urls.json');
+
 /* GET home page. */
 router.get('/tags', function(req, res, next) {
-  get({
-    host: 'api.github.com',
-    port: 443,
-    path: '/repos/elastic/elasticsearch/tags?per_page=100',
-    headers: {
-      'user-agent': 'spalger'
-    }
-  })
-  .on('response', function (incoming) {
-    incoming.pipe(res);
-  }).on('error', next);
+  res.sendFile(urlsPath);
 });
 
 module.exports = router;
